a29f23cbfe8dc2392d62736772568fde83642367
Bhavik <dalsaniyabhavik@gmail.com>
2024-12-27 13:13:49 +0530

    static crud operations

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-const path = require("path");
-const fs = require("fs");
-console.log(__dirname);
-const dirPath = path.join(__dirname, "files");
+const mongoose = require("mongoose");
 
-for (i = 0; i < 5; i++) {
-  fs.writeFileSync(`${dirPath}/hello${i}.txt`, "Hello world!!");
-}
+mongoose.connect("mongodb://localhost:27017/e-comm");
 
-fs.readdir(dirPath, (err, files) => {
-  files.forEach((file) => {
-    console.log(`file name is ${file}`);
-  });
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  brand: String,
+  category: String,
 });
+
+const saveInDB = async () => {
+  const Product = mongoose.model("product", productSchema);
+  const data = new Product({
+    name: "iphone 16",
+    price: 100000,
+    brand: "apple",
+    category: "mobile",
+  });
+  const result = await data.save();
+  console.log(result);
+};
+
+// saveInDB();
+
+const updateInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  const data = await Product.updateOne(
+    { name: "iphone 16" },
+    { $set: { price: 60000 } }
+  );
+  console.log(data);
+};
+
+// updateInDB();
+
+const deleteInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+
+  const data = await Product.deleteOne({ name: "iphone 16" });
+  console.log(data);
+};
+
+// deleteInDB();
+
+const findInDB = async () => {
+  const Product = mongoose.model("products", productSchema);
+  const data = await Product.find();
+  console.log(data);
+};
+
+findInDB();
