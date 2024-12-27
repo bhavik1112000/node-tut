8ebad2aa0968424a2edbaab92d0e2c46d7c24345
Bhavik <dalsaniyabhavik@gmail.com>
2024-12-27 16:40:50 +0530

    search with multiple fields

--- index.js
+++ index.js
@@ -6,31 +6,14 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/create", async (req, res) => {
-  const data = new product(req.body);
-  let result = await data.save();
-  // res.send("done");
-  res.send(result);
-
-  // console.log(req.body);
-  console.log(result);
-});
-
-app.get("/products", async (req, res) => {
-  const data = await product.find();
+app.get("/search/:key", async (req, res) => {
+  const data = await product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+    ],
+  });
   res.send(data);
-});
-
-app.delete("/delete/:_id", async (req, res) => {
-  const data = await product.deleteOne(req.params);
-  console.log(data);
-  res.send(data);
-});
-
-app.put("/update/:_id", async (req, res) => {
-  const data = await product.updateOne(req.params, { $set: req.body });
-  res.send(data);
-  console.log(data);
 });
 
 app.listen("5000");
